38e922f8e7f536b931f3faf119204e133883e6dd
HoangLV-UD <98141013+HoangLV-UD@users.noreply.github.com>
2023-04-01 00:18:07 +0700

    commit 1 01/04/2023

---
+++ src/main/java/com/example/demo/dao/RoleDao.java
@@ -0,0 +1,7 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleDao extends JpaRepository<Role, String> {
+}
