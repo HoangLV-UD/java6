9434810603ee710abde3e18f54bf2edadab16467
HoangLV-UD <98141013+HoangLV-UD@users.noreply.github.com>
2023-04-12 22:26:37 +0700

    update 12/04/2023

--- src/main/java/com/example/demo/controller/ShoppingCartController.java
+++ src/main/java/com/example/demo/controller/ShoppingCartController.java
@@ -11,15 +11,4 @@ public class ShoppingCartController {
 
     }
 
-    @FunctionalInterface
-
-    public interface Demo4Inter{
-
-        void m1(int x);
-
-    }
-    Demo4Inter o4 = x -> System.out.println(x);
-    Demo4Inter o1 = new Demo4Inter() { @Override public void m1(int x) {} };
-    Demo4Inter o3 = x -> {};
-    Demo4Inter o2 = new Demo4Inter() { @Override public void m1(int x) {} public void m2() {} };
 }
