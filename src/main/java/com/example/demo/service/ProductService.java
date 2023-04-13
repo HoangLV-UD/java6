aef695e65251eea26cf30aa02514589ba635e32a
locxp <locxp@lifetek.vn>
2023-04-13 14:06:59 +0700

    update 13/04/2023

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductService extends PagingAndSortingRepository {
+public interface ProductService  {
     List<Product> findAll();
 
     Product findById(Integer id);
--- src/main/resources/static/assets/js/shopping-cart-app.js
+++ src/main/resources/static/assets/js/shopping-cart-app.js
@@ -1,6 +1,5 @@
 const app = angular.module("shopping-cart-app",[]);
 app.controller("shopping-cart-ctrl", function( $scope, $http){
-    alert("AngularJs")
     $scope.cart = {
         items: [],
         add(id){
@@ -8,17 +7,26 @@ app.controller("shopping-cart-ctrl", function( $scope, $http){
           if(item){
             item.qty++;
             this.saveToLocalStorage();
+            alert("Success")
           }
           else{
             $http.get(`/api/rest/product/${id}`).then(resp => {
                 resp.data.qty = 1;
                 this.items.push(resp.data);
-                this.saveToLocalStorage();  
+                this.saveToLocalStorage(); 
+                alert("Success") 
             })
           }
         },
-        remove(id){},
-        clear(){},
+        remove(id){
+          var index = this.items.findIndex(item => item.id == id); //tim sp theo id
+          this.items.splice(index,1); //splice dùng để thêm hoặc loại bỏ phần tử xong mảng
+          this.saveToLocalStorage();
+        },
+        clear(){
+            this.items = []; //set mảng sp thành mảng rỗng 
+            this.saveToLocalStorage(); // lưu
+        }, 
         atm_of(item){},
         get count(){
             return this.items
@@ -33,6 +41,12 @@ app.controller("shopping-cart-ctrl", function( $scope, $http){
         saveToLocalStorage(){
             var json = JSON.stringify(angular.copy(this.items));
             localStorage.setItem("cart",json);
+            
         },
+        loadFromLocalStorage(){
+          var json = localStorage.getItem("cart");
+          this.items = json ? JSON.parse(json) : [];
+        }
     }
+    $scope.cart.loadFromLocalStorage();
 })
