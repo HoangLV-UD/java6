77714560063c9209559084166a94c534413a2006
locxp <locxp@lifetek.vn>
2023-04-13 14:18:32 +0700

    tesst commit in Inteliji

--- src/main/resources/static/assets/js/shopping-cart-app.js
+++ src/main/resources/static/assets/js/shopping-cart-app.js
@@ -1,52 +1,52 @@
-const app = angular.module("shopping-cart-app",[]);
-app.controller("shopping-cart-ctrl", function( $scope, $http){
-    $scope.cart = {
-        items: [],
-        add(id){
-          var item = this.items.find(item => item.id == id);
-          if(item){
-            item.qty++;
-            this.saveToLocalStorage();
-            alert("Success")
-          }
-          else{
-            $http.get(`/api/rest/product/${id}`).then(resp => {
-                resp.data.qty = 1;
-                this.items.push(resp.data);
-                this.saveToLocalStorage(); 
-                alert("Success") 
-            })
-          }
-        },
-        remove(id){
-          var index = this.items.findIndex(item => item.id == id); //tim sp theo id
-          this.items.splice(index,1); //splice dùng để thêm hoặc loại bỏ phần tử xong mảng
+const app = angular.module("shopping-cart-app", []);
+app.controller("shopping-cart-ctrl", function ($scope, $http) {
+  $scope.cart = {
+    items: [],
+    add(id) {
+      var item = this.items.find(item => item.id == id);
+      if (item) {
+        item.qty++;
+        this.saveToLocalStorage();
+        alert("Success")
+      }
+      else {
+        $http.get(`/api/rest/product/${id}`).then(resp => {
+          resp.data.qty = 1;
+          this.items.push(resp.data);
           this.saveToLocalStorage();
-        },
-        clear(){
-            this.items = []; //set mảng sp thành mảng rỗng 
-            this.saveToLocalStorage(); // lưu
-        }, 
-        atm_of(item){},
-        get count(){
-            return this.items
-            .map(item => item.qty)
-            .reduce((total,qty) => total += qty, 0)
-        },
-        get amount(){
-            return this.items
-            .map(item => item.qty * item.price)
-            .reduce((total,qty) => total += qty, 0)
-        },
-        saveToLocalStorage(){
-            var json = JSON.stringify(angular.copy(this.items));
-            localStorage.setItem("cart",json);
-            
-        },
-        loadFromLocalStorage(){
-          var json = localStorage.getItem("cart");
-          this.items = json ? JSON.parse(json) : [];
-        }
+          alert("Success")
+        })
+      }
+    },
+    remove(id) {
+      var index = this.items.findIndex(item => item.id == id); //tim sp theo id
+      this.items.splice(index, 1);
+      this.saveToLocalStorage();
+    },
+    clear() {
+      this.items = []; //set mảng sp thành mảng rỗng 
+      this.saveToLocalStorage(); // lưu
+    },
+    atm_of(item) { },
+    get count() {
+      return this.items
+        .map(item => item.qty)
+        .reduce((total, qty) => total += qty, 0)
+    },
+    get amount() {
+      return this.items
+        .map(item => item.qty * item.price)
+        .reduce((total, qty) => total += qty, 0)
+    },
+    saveToLocalStorage() {
+      var json = JSON.stringify(angular.copy(this.items));
+      localStorage.setItem("cart", json);
+
+    },
+    loadFromLocalStorage() {
+      var json = localStorage.getItem("cart");
+      this.items = json ? JSON.parse(json) : [];
     }
-    $scope.cart.loadFromLocalStorage();
+  }
+  $scope.cart.loadFromLocalStorage();
 })
